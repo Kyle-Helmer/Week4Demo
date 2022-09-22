7e7d39db90793ef949dc40d06b0d24d33dabdd7b
Kyle-Helmer <99497831+Kyle-Helmer@users.noreply.github.com>
2022-09-22 09:07:52 -0600

    Hello World working Base version with no validation

---
+++ src/java/servlets/HelloWorldServlet.java
@@ -0,0 +1,43 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ *
+ * @author super
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
+                .forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Retrieving what the user input into the text boxes for their name
+        //And passing it to a new variable to be used within the JSP
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        
+        request.setAttribute("firstname", firstname);
+        request.setAttribute("lastname", lastname);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
+                .forward(request, response);
+        
+        
+        
+    }
+
+   
+
+}
---
+++ web/WEB-INF/sayHello.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : sayHello
+    Created on : 22-Sep-2022, 9:03:20 AM
+    Author     : super
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hello Wurld</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <h2>Hello ${firstname} ${lastname}</h2>
+    </body>
+</html>
